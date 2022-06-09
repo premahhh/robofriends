b1c76fa139506a59a3f4622a89601d5590fc4a83
Preston Prince <prestonprince18@gmail.com>
2022-06-09 16:48:56 -0600

    Updates

---
+++ precache-manifest.2222d6b4b06df365dc2d6b326ac2c2cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "13eb235c06a5c7cbcf70",
+    "url": "/robofriends/static/css/main.bc84bd6b.chunk.css"
+  },
+  {
+    "revision": "13eb235c06a5c7cbcf70",
+    "url": "/robofriends/static/js/main.13eb235c.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "d3abe282fc6d429b1465",
+    "url": "/robofriends/static/css/2.68891aba.chunk.css"
+  },
+  {
+    "revision": "d3abe282fc6d429b1465",
+    "url": "/robofriends/static/js/2.d3abe282.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "92102d10ebcc8c76727c015acff7bd62",
+    "url": "/robofriends/index.html"
+  }
+];
